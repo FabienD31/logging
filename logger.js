10f66f05f679626232bec0ef123d4250d85776a6
fabien desnoues <f.desnoues@keabot.fr>
2021-12-14 14:40:34 +0100

    new format to sort logs in files

--- logger.js
+++ logger.js
@@ -1,14 +1,10 @@
 const winston = require('winston');
-const {ElasticsearchTransport} = require('winston-elasticsearch');
+// const {ElasticsearchTransport} = require('winston-elasticsearch');
 
+const customFormat = winston.format.printf(i => {
+	return `${i.level.toUpperCase()}: ${i.timestamp} ${i.message}`;
+});
 
-const levels = {
-  error: 0,
-  warn: 1,
-  info: 2,
-  http: 3,
-  debug: 4,
-}
 const colors = {
     error: 'red',
     warn: 'yellow',
@@ -16,59 +12,77 @@ const colors = {
     http: 'magenta',
     debug: 'white',
 };
-const level = () => {
-  const env = process.env.NODE_ENV || 'development'
-  const isDevelopment = env === 'development'
-  return isDevelopment ? 'debug' : 'warn'
-}
+
 winston.addColors(colors);
 
 
+const infoAndWarnFilter = winston.format((info, opts) => { 
+	return info.level === 'info' || info.level === 'warn' ? info : false
+})
+const httpFilter = winston.format((info, opts) => {
+  return info.level === 'http' ? info : false
+})
 
-// pour ElasticSearch
-const esTransportOpts = {
-    levels: 'error',
-    clientOpts: { node: 'http://localhost:9200' }
-};
- 
+const errorFilter = winston.format((info, opts) => { 
+	return info.level === 'error' ? info : false 
+})
 
+var transports = [
+	new (winston.transports.File)({
+		name: 'Error Logs',
+		filename: './logs/error.log',
+		datePattern: 'YYYY-MM-DD',
+		maxSize: '128m',
+		maxFiles: '14d',
+		level: 'warn',
+		format: winston.format.combine(
+			errorFilter(),
+			winston.format.timestamp(),
+			customFormat
+		)
+	}),
+	new (winston.transports.File)({
+		name: 'INFO logs',
+		filename: './logs/all.log',
+		datePattern: 'YYYY-MM-DD',
+		maxSize: '128m',
+		maxFiles: '14d',
+		level: 'info',
+		format: winston.format.combine(
+			infoAndWarnFilter(),
+			winston.format.timestamp(),
+			customFormat
+		)
+  }),
+  	new (winston.transports.File)({
+		name: 'HTTP logs',
+		filename: './logs/http.log',
+		datePattern: 'YYYY-MM-DD',
+		maxSize: '128m',
+		maxFiles: '14d',
+		level: 'http',
+		format: winston.format.combine(
+			httpFilter(),
+			winston.format.timestamp(),
+			customFormat
+		)
+	}),
+	new (winston.transports.Console)({		
+    level: 'http',
+		json: false,
+		format: winston.format.combine(
+			winston.format.colorize(),
+			winston.format.simple()
+		)
+	})
+]
 
-// winston.add(new winston.transports.File({ filename: 'error.log' }));
-
-
-
-const format = winston.format.combine(
-    winston.format.json(),
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
-    winston.format.colorize({ all: true }),
-    // winston.format.printf(
-    //     (info) => `${info.timestamp} ${info.level}: ${info.message}`,
-    //     ),
-        )
-        
-        const transports = [
-            new winston.transports.Console(),
-            new winston.transports.File({
-                filename: 'logs/error.log',
-                level: 'error',
-            }),
-            new winston.transports.File({ filename: 'logs/all.log' }),
-            new winston.transports.File({ filename: 'logs/http.log',  level: 'http' }),
-            
-            new ElasticsearchTransport(esTransportOpts)        
-        ]
-        
-        const Logger = winston.createLogger({
-            level: level(),
-            levels,
-            format,
-            transports,
-        })
-
-
-Logger.on('error', (error) => {
-  console.error('Error in logger caught', error);
-});
-
-
-module.exports = Logger;
+var logger = winston.createLogger({
+	transports: transports,
+	exitOnError: false,
+	format: winston.format.combine(
+		winston.format.timestamp(),
+		customFormat
+	)
+})
+module.exports = logger;
